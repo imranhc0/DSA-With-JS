a924ba4375f0aedd007650e04d88acde3c2279d6
Mohammad Imran Hossen <imranhosein.cse@gmail.com>
2022-11-03 18:40:02 +0600

    PS/BS--> Array Reverse

---
+++ Problem Solving/Basic Problem/ArrayReverse.js
@@ -0,0 +1,16 @@
+// Reverse a Array
+// I/O
+// [1,2,3,4] == [4,3,2,1]
+
+
+function revArray(arr){
+    return arr.reverse() //Most Simplier way to reverse a array
+    // let revArr = []
+    // for(let i = 0; i <= arr.length-1; i ++){
+    //     revArr.unshift(arr[i])
+
+    // }
+    // return revArr
+}
+
+console.log(revArray([1,2,3,4]))
